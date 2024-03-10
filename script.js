913f793d8ea41b453a57cab3ed7d71f9f8805a3a
darialiit <solomkina.daria@gmail.com>
2024-03-10 17:00:39 -0300

    Jest - using Babel

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-// class Ship {
-//     length = 3,
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hit = 0;
+    this.sunk = false;
+  }
+  toHit() {
+    return this.hit++;
+  }
+  isSunk() {
+    if (this.hit === this.length) {
+      return true;
+    }
+  }
+}
 
-// }
+const t = new Ship(5);
+t.toHit();
+t.toHit();
+t.toHit();
+t.isSunk();
+console.log(t);
+t.toHit();
+t.toHit();
+t.isSunk();
+
+console.log(t);
